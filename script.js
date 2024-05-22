5ae8dc16826c4b6d04db736cf04a2768ca6d8f09
cesar-olliveira <cesarolliveira25@gmail.com>
2024-05-21 22:20:17 -0300

    Atualizações em geral

--- script.js
+++ script.js
@@ -1,59 +1,19 @@
-const canvas = document.getElementById('networkCanvas');
-const ctx = canvas.getContext('2d');
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const numNodes = 100;
-const nodes = [];
-
-class Node {
-    constructor(x, y, radius, speed) {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.speed = speed;
-        this.directionX = Math.random() * speed - speed / 2;
-        this.directionY = Math.random() * speed - speed / 2;
-    }
-
-    draw() {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
-        ctx.fill();
-    }
-
-    update() {
-        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
-            this.directionX = -this.directionX;
-        }
-        if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
-            this.directionY = -this.directionY;
-        }
-        this.x += this.directionX;
-        this.y += this.directionY;
-        this.draw();
-    }
+// Função para rolar até o topo da página
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
-function init() {
-    for (let i = 0; i < numNodes; i++) {
-        const radius = Math.random() * 3 + 1;
-        const x = Math.random() * (canvas.width - radius * 2) + radius;
-        const y = Math.random() * (canvas.height - radius * 2) + radius;
-        const speed = 1;
-        nodes.push(new Node(x, y, radius, speed));
-    }
-}
+// Exibir o botão apenas quando o id da seção atual não for "Home"
+window.onscroll = function() {
+    let button = document.getElementById('backToTopBtn');
+    let homeSection = document.getElementById('Home');
+    let servicosSection = document.getElementById('Servicos');
+    let homePosition = homeSection.getBoundingClientRect().top;
+    let servicosPosition = servicosSection.getBoundingClientRect().top;
 
-function animate() {
-    requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < nodes.length; i++) {
-        nodes[i].update();
+    if (homePosition < 0 || homePosition >= window.innerHeight) {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
     }
-}
-
-init();
-animate();
+};
